741c57c0f08137e3ac3713c78452d1ddab185ed9
Alex Schokking <aschokking@gmail.com>
2014-07-22 23:27:49 -0700

    getting templates working, added project file

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from polls.models import Poll
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world! You're at the poll index, welcome")
-    
+  latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+  context = { "latest_poll_list": latest_poll_list}
+  return render(request, 'polls/index.html', context)
+  
 def detail(request, poll_id):
-    return HttpResponse("Detail for poll %s" % poll_id)
+  return HttpResponse("Detail for poll %s" % poll_id)
     
 def results(request, poll_id):
-    return HttpResponse("Results of poll %s" % poll_id)
+  return HttpResponse("Results of poll %s" % poll_id)
     
 def vote(request, poll_id):
-    return HttpResponse("Voting on poll %s" % poll_id)
+  return HttpResponse("Voting on poll %s" % poll_id)
